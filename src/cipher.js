85d2c0db8a5b92f447aaa476763245a5634c5b01
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2019-06-17 20:10:09 -0400

    Second commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,69 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+
+  encode: (offset,message) => {
+   //console.log(offset,message);
+   //declaro una variable vacía que me guardará el resultado de las letras convertidas, que es string
+    let newMessage = ""; 
+    // creo un loop para recorrer cada letra/string del array para transformarlo a número ASCII
+    for (let i = 0; i < message.length; i++){ 
+    //transformo cada letra en su número ASCII, [i] llama a todos los elementos del array
+      let positionAscii= message.charCodeAt(i); 
+      //console.log(positionAscii);
+         
+      //condiciono si la letra está entre el espacio 65 y 90 que corresponde de la A a la Z y si es true aplico la fórmula
+      if (positionAscii >= 65 && positionAscii <= 90){
+        let cifrado = positionAscii - 65 + offset % 26 + 65;
+        //El método .String transforma en string cada número
+        // al poner += concateno cada string del nuevo arreglo
+        newMessage += String.fromCharCode(cifrado);
+      }
+
+      //integrar espacio
+      else if (positionAscii === 32){
+        newMessage += ' ';
+      }
+
+      //integrar números
+      else if (positionAscii >= 48 && positionAscii <= 57){
+        let cifrado = positionAscii- 48 + offset % 10 + 48;
+        newMessage += String.fromCharCode(cifrado);
+      }          
+    }
+    //console.log(newMessage)
+    return newMessage;     
   },
-  decode: () => {
-    /* Acá va tu código */
+
+
+  decode: (offset,message) => {
+
+      let newMessage = ""; 
+        for (let i = 0; i < message.length; i++){ 
+            let positionAscii = message.charCodeAt(i); 
+
+            //letras mayúsculas
+            if (positionAscii >= 65 && positionAscii <= 90 ){
+              let descifrado = (positionAscii - 90 - offset) % 26 + 90;
+              newMessage += String.fromCharCode(descifrado);
+            }
+
+            //espacio
+            else if (positionAscii === 32){
+            newMessage += ' ';
+            }
+
+             //numeros
+            else if (positionAscii >= 48 && positionAscii <= 57){
+            let descifrado = (positionAscii - 57 - offset) %10 + 57;
+            newMessage += String.fromCharCode(descifrado);
+             }          
+      }
+      return newMessage;     
   }
 };
+ 
+
+
+
+
+
+     
--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-/*Acá va tu código*/
+
+//Llamo al botón de Codificar para aplicarle una función al hacer "click"
+let btnEncode = document.getElementById('btn-encode');
+    btnEncode.addEventListener('click', () => {
+        window.message = document.getElementById('user-message').value;
+        window.message = message.toUpperCase ();
+        window.offset = document.getElementById('user-offset').value;
+
+        let newMessage = window.cipher.encode(offset,message);
+        document.getElementById('final-message').innerHTML = newMessage;
+    });
+
+
+let btnDecode = document.getElementById("btn-decode");
+    btnDecode.addEventListener('click', () => {
+        window.message = document.getElementById('user-message').value;
+        window.offset = document.getElementById('user-offset').value;
+        window.message = message.toUpperCase ();
+
+        let newMessage = cipher.decode(offset,message);
+        document.getElementById('final-message').innerHTML = newMessage;
+    });
+
+/*
+//hacer que el botón limpiar haga algo
+//document.getElementById("btn-clean").addEventListener('click'), ()=> {
+let btnDelete = document.getElementById("btn-clean");
+    btnDelete.addEventListener('click', () => {
+        message.value = "";
+    });
+*/
